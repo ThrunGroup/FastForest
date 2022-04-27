7f902c5359da92949d566ac33cf70a04f18c9907
vxbrandon <vxbrandon@gmail.com>
2022-04-27 14:15:33 +0900

    Add wandb and imblearn(imbalanced_learn) modules to requirements.txt

--- heart_experiments/compare_experiment.py
+++ heart_experiments/compare_experiment.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.base import clone
 from sklearn.metrics import balanced_accuracy_score, precision_score
 
-from fit_heart import split_data, load_data
+from heart_experiments.fit_heart import split_data, load_data
 from forest import Forest
 
 
