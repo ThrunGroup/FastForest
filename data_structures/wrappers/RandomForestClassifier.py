3f8a8063ef7673f2e9eaaf728c42dae648c248ac
motiwari <mohittiwarinyc+github@gmail.com>
2022-05-10 17:23:42 -0700

    Adding RandomForestClassifier and RandomForestRegressor wrappers

---
+++ data_structures/wrappers/RandomForestClassifier.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from data_structures.forest_classifier import ForestClassifier
+from utils.constants import SQRT, IDENTITY, GINI, BEST, EXACT
+
+
+def RandomForestClassifier(ForestClassifier):
+    """
+    A RandomForestClassifier, which is a ForestClassifier with the following settings:
+
+    bootstrap: bool = True,
+    feature_subsampling: str = SQRT,
+    tree_global_feature_subsampling: bool = False,
+    bin_type: str = IDENTITY,
+    criterion: str = GINI,
+    splitter: str = BEST,
+    solver: str = EXACT,
+    """
+
+    def __init__(
+        self,
+        data: np.ndarray = None,
+        labels: np.ndarray = None,
+        n_estimators: int = 100,
+        max_depth: int = None,
+        min_samples_split: int = 2,
+        min_impurity_decrease: float = 0,
+        max_leaf_nodes: int = None,
+        budget: int = None,
+        criterion: str = GINI,
+        splitter: str = BEST,
+        solver: str = EXACT,
+        random_state: int = 0,
+        verbose: bool = False,
+    ) -> None:
+        super().__init__(
+            data=data,
+            labels=labels,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            bootstrap=True,  # Fixed
+            feature_subsampling=SQRT,  # Fixed
+            tree_global_feature_subsampling=False,  # Fixed
+            min_samples_split=min_samples_split,
+            min_impurity_decrease=min_impurity_decrease,
+            max_leaf_nodes=max_leaf_nodes,
+            bin_type=IDENTITY,  # Fixed
+            num_bins=None,  # Fixed
+            budget=budget,
+            criterion=criterion,
+            splitter=splitter,
+            solver=solver,
+            erf_k=None,  # Fixed
+            random_state=random_state,
+            verbose=verbose,
+        )
---
+++ data_structures/wrappers/RandomForestRegressor.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+from data_structures.forest_regressor import ForestRegressor
+from utils.constants import SQRT, IDENTITY, GINI, BEST, EXACT, MAB
+
+
+def RandomForestClassifier(ForestRegressor):
+    """
+    A RandomForestClassifier, which is a ForestClassifier with the following settings:
+
+    data: np.ndarray = None,
+    labels: np.ndarray = None,
+    n_estimators: int = 100,
+    max_depth: int = None,
+    bootstrap: bool = True,
+    feature_subsampling: str = None,
+    tree_global_feature_subsampling: bool = False,
+    min_samples_split: int = 2,
+    min_impurity_decrease: float = 0,
+    max_leaf_nodes: int = None,
+    bin_type: str = LINEAR,
+    num_bins: int = DEFAULT_NUM_BINS,
+    budget: int = None,
+    criterion: str = GINI,
+    splitter: str = BEST,
+    solver: str = MAB,
+    erf_k: str = SQRT,
+    random_state: int = 0,
+    verbose: bool = False,
+    """
+
+    def __init__(
+        self,
+        data: np.ndarray = None,
+        labels: np.ndarray = None,
+        n_estimators: int = 100,
+        max_depth: int = None,
+        min_samples_split: int = 2,
+        min_impurity_decrease: float = 0,
+        max_leaf_nodes: int = None,
+        budget: int = None,
+        criterion: str = GINI,
+        splitter: str = BEST,
+        solver: str = EXACT,
+        random_state: int = 0,
+        verbose: bool = False,
+    ) -> None:
+        super().__init__(
+            self,
+            data=data,
+            labels=labels,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            bootstrap=True,  # Fixed
+            feature_subsampling=SQRT,  # Fixed
+            tree_global_feature_subsampling=False,  # Fixed
+            min_samples_split=min_samples_split,
+            min_impurity_decrease=min_impurity_decrease,
+            max_leaf_nodes=max_leaf_nodes,
+            bin_type=IDENTITY,  # Fixed
+            num_bins=None,  # Fixed
+            budget=budget,
+            criterion=criterion,
+            splitter=splitter,
+            solver=solver,
+            erf_k=None,  # Fixed
+            random_state=random_state,
+            verbose=verbose,
+        )
