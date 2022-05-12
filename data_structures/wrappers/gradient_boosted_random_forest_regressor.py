4a5ab2b14b6818876e7cece30119ecde56e9ba85
motiwari <mohittiwarinyc+github@gmail.com>
2022-05-11 20:07:23 -0700

    Implementing all remaining wrappers

--- data_structures/wrappers/gradient_boosted_random_forest_regressor.py
+++ data_structures/wrappers/gradient_boosted_random_forest_regressor.py
@@ -1,3 +1,63 @@
-class GradientBoostedRandomForestRegressor:
-    def __init__(self):
-        raise NotImplementedError("Not done yet")
+import numpy as np
+
+from data_structures.forest_regressor import ForestRegressor
+from utils.constants import SQRT, IDENTITY, BEST, EXACT, MSE
+
+
+class GradientBoostedRandomForestRegressor(ForestRegressor):
+    """
+    A GradientBoostedRandomForestRegressor, which is a ForestRegressor with the following settings:
+
+    bootstrap: bool = True,
+    feature_subsampling: str = SQRT,
+    tree_global_feature_subsampling: bool = False,
+    bin_type: str = IDENTITY,
+    num_bins: int = None,
+    solver: str = EXACT (default value, not fixed, but cannot use MAB because there's no binning)
+    boosting: bool = True,
+    boosting_lr: float = passed parameter
+    """
+
+    def __init__(
+        self,
+        data: np.ndarray = None,
+        labels: np.ndarray = None,
+        n_estimators: int = 100,
+        max_depth: int = None,
+        min_samples_split: int = 2,
+        min_impurity_decrease: float = 0,
+        max_leaf_nodes: int = None,
+        budget: int = None,
+        criterion: str = MSE,
+        splitter: str = BEST,
+        solver: str = EXACT,
+        random_state: int = 0,
+        verbose: bool = False,
+        boosting_lr: float = None,
+    ) -> None:
+        if boosting_lr is None:
+            raise Exception(
+                "Must pass boosting_lr to GradientBoostedRandomForestRegressor"
+            )
+        super().__init__(
+            data=data,
+            labels=labels,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            bootstrap=True,  # Fixed
+            feature_subsampling=SQRT,  # Fixed
+            tree_global_feature_subsampling=False,  # Fixed
+            min_samples_split=min_samples_split,
+            min_impurity_decrease=min_impurity_decrease,
+            max_leaf_nodes=max_leaf_nodes,
+            bin_type=IDENTITY,  # Fixed
+            num_bins=None,  # Fixed
+            budget=budget,
+            criterion=criterion,
+            splitter=splitter,
+            solver=solver,
+            random_state=random_state,
+            verbose=verbose,
+            boosting=True,  # Fixed
+            boosting_lr=boosting_lr,  # Fixed
+        )
