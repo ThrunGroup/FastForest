891ff8f4b15e95aba991300e43f00940ae267a9a
motiwari <mohittiwarinyc+github@gmail.com>
2022-05-14 15:31:05 -0700

    Codifying initial exploration from jpnyb into initial_exploration.py

---
+++ experiments/compare_baseline_implementations.py
@@ -0,0 +1,19 @@
+from data_structures.wrappers.random_forest_classifier import (
+    RandomForestClassifier as RFC_ours,
+)
+from data_structures.wrappers.extremely_random_forest_classifier import (
+    ExtremelyRandomForestClassifier as ERFC_ours,
+)
+
+from data_structures.wrappers.random_forest_regressor import (
+    RandomForestRegressor as RFR_ours,
+)
+from data_structures.wrappers.extremely_random_forest_regressor import (
+    ExtremelyRandomForestRegressor as ERFR_ours,
+)
+
+from sklearn.ensemble import RandomForestClassifier as RFC_sklearn
+from sklearn.ensemble import ExtraTreesClassifier as ERFC_sklearn
+
+from sklearn.ensemble import RandomForestRegressor as RFR_sklearn
+from sklearn.ensemble import ExtraTreesRegressor as ERFR_sklearn
