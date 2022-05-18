590520eaffd405d0f65f5eb17858a5461b69e6cc
motiwari <mohittiwarinyc+github@gmail.com>
2022-05-17 18:36:58 -0700

    Adding all results up to 280k for withOUT replacement

--- experiments/snappy_analysis/analyze_output.py
+++ experiments/snappy_analysis/analyze_output.py
@@ -4,7 +4,7 @@ import numpy as np
 def main():
     with open("../results_filtered.csv") as fin:
         sizes = []
-        results = np.zeros((6, 20))
+        results = np.zeros((12, 20))
         size_idx = 0
         rc_idx = 0
         print(results)
@@ -15,18 +15,18 @@ def main():
             elif "Results for" in line:
                 size_idx += 1
                 rc_idx = 0
-                sizes.append(line.strip().split()[-2])
+                sizes.append(int(line.strip().split()[-2]))
 
     if rc_idx != 9:
         print("Warning: last row has incomplete results")
 
     print(results)
     print()
-    print(sizes)
+    print(list(sizes))
     print()
-    print(np.mean(results, axis=1))
+    print(list(np.mean(results, axis=1)))
     print()
-    print(np.std(results, axis=1))
+    print(list(np.std(results, axis=1)))
 
 
 if __name__ == "__main__":
