4e252266be6bd9920962ddc24ba15b5422da3b99
motiwari <mohittiwarinyc+github@gmail.com>
2022-05-26 02:37:23 +0200

    Fixing merge conflict with investigate_scaling.py

--- experiments/scaling_exps/investigate_scaling.py
+++ experiments/scaling_exps/investigate_scaling.py
@@ -2,39 +2,23 @@ from mnist import MNIST
 
 from experiments.runtime_exps.compare_runtimes import *
 
-import pprint
-
 
 def main():
     mndata = MNIST("mnist/")
 
     train_images, train_labels = mndata.load_training()
 
-    # For accuracy comparison. Looks ok
-    # test_images, test_labels = mndata.load_testing()
-    # test_images = np.array(test_images)
-    # test_labels = np.array(test_labels)
-
     for C_SUBSAMPLE_SIZE in [
+        5000,
         10000,
         20000,
-        30000,
         40000,
-        50000,
         60000,
-        80000,
-        100000,
-        120000,
-        140000,
-        160000,
-        180000,
-        200000,
-        220000,
-        240000,
-        260000,
-        280000,
-        300000,
-        320000,
+        # 80000,
+        # 160000,
+        # 320000,
+        # 640000,
+        # 1280000,
     ]:
         print("\n\n")
         for fitting_seed in range(100, 105):
@@ -45,19 +29,15 @@ def main():
             train_labels_subsampled = np.array(train_labels)[idcs]
             compare_runtimes(
                 "HRFC",
-                full_train_data=train_images,
-                full_train_targets=train_labels,
-                test_data=None,
-                test_targets=None,
-                starting_seed=100,
-                num_seeds=20,
+                train_images_subsampled,
+                train_labels_subsampled,
                 predict=False,
                 run_theirs=False,
-                profile_name="HRFC_" + str(C_SUBSAMPLE_SIZE) + "_profile_",
-                C_SUBSAMPLE_SIZE=C_SUBSAMPLE_SIZE,
+                profile_name="HRFC_"
+                + str(C_SUBSAMPLE_SIZE)
+                + "_profile_"
+                + str(fitting_seed),
             )
-        )
-        print("Results for ", C_SUBSAMPLE_SIZE, " above")
 
         # compare_runtimes(
         #     "ERFC",
